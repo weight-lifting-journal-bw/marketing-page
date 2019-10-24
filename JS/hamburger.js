b435c17835826555d1955e02ae2b6c5a4526132f
Sebastian Ojefors <godnoken@gmail.com>
2019-10-24 18:06:06 +0200

    Last changes

--- JS/hamburger.js
+++ JS/hamburger.js
@@ -3,13 +3,7 @@ let menu = document.querySelector(".hamburgerMenu");
 let maxWidth = window.matchMedia( "(max-width: 1000px)" );
 
 
-// hamburger.addEventListener('click', openMenu);
-
-function openMenu () {
-    if(maxWidth === true){
-        hamburger.addEventListener('click', openMenu);
-    }
-}
+hamburger.addEventListener('click', openMenu);
 
 function openMenu () {
         if (menu.style.display === "none"){
